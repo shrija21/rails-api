3c69aa96f914a28c047984453d777a64f73341d4
shri <shrija.tumma1994@gmail.com>
2020-05-05 20:13:12 +0530

    Serializing Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
 	validates :title, presence: true
 	validates :content, presence: true
-	validates :slug, presence: true, uniqueness: true
+	validates :slug, presence: true, uniqueness: { case_sensitive: true }
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -8,11 +8,21 @@ describe ArticlesController do
 		end
 
 		it 'should return proper json' do
+			create_list :article, 2
 			get :index
 			json = JSON.parse(response.body)
 			p json
-			# json_data = json[:data]
-			# expect(json_data.length).to eq(2)
+			expect(json.length).to eq(2)
+			expect(json[0]).to include({
+				"title"=>"My awesome article 1", 
+				"content"=>"The content of my awesome article 1", 
+				"slug"=>"my-awesome-article-1"
+			})
+			expect(json[1]).to include({
+				"title"=>"My awesome article 2", 
+				"content"=>"The content of my awesome article 2", 
+				"slug"=>"my-awesome-article-2"
+			})
 		end
 	end
 end
